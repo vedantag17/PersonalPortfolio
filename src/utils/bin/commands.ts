0f0ca60d7cb2c60abf9885186bee020d3931b22b
Vedant Agrawal <vrsagrawal@gmail.com>
2024-03-02 18:19:11 +0530

    Fixed errors

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -39,17 +39,12 @@ More about me:
 'readme' - my github readme.`;
 };
 
-export const resume = async (args: string[]): Promise<string> => {
-  window.open(`${config.resume_url}`);
-  return 'Opening resume...';
-};
 
 // Donate
 export const donate = async (args: string[]): Promise<string> => {
   return `thank you for your interest. 
 here are the ways you can support my work:
 - <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
 `;
 };
 
